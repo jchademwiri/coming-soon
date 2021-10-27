768fa9f532ecec8147c7bd87a6af01aef261146b
Jacob Chademwiri <jchademwiri@gmail.com>
2021-10-27 10:32:00 +0200

    -initial commit

--- pages/index.js
+++ pages/index.js
@@ -3,32 +3,30 @@ import Head from 'next/head';
 import ComingSoon from '../components/ComingSoon';
 import Footer from '../components/Footer';
 
-import styles from '../styles/Home.module.scss';
-
 export default function Home() {
 	return (
 		<div>
 			<Head>
-				<title>Playhouse Media - Home</title>
+				<title>Playhouse Media Group - Home</title>
 				<meta
 					name='description'
-					content='Playhouse Media Is An Award-Winning Digital Marketing Agency. We offer your Business the chance to build a Successful Digital Marketing Strategy'
+					content='Playhouse Media Group Is An Award-Winning Digital Marketing Agency. We offer your Business the chance to build a Successful Digital Marketing Strategy'
 				/>
 				<link rel='canonical' href='https://www.playhousemedia.net' />
 				<meta property='og:locale' content='en_US' />
 				<meta property='og:type' content='website' />
 				<meta
 					property='og:title'
-					content='Playhouse Media - Digital Marketing Agency'
+					content='Playhouse Media Group - Digital Marketing Agency'
 				/>
 				<meta
 					property='og:description'
-					content='Playhouse Media takes pride in having worked with and advised many Business Start-Ups, Entrepreneurs, and Established Businesses Globally on their Strategic Marketing Initiatives. We welcome the opportunity to offer your Business the chance to build a successful Marketing Strategy to better dominate your market segment quickly and effectively.'
+					content='Playhouse Media Group takes pride in having worked with and advised many Business Start-Ups, Entrepreneurs, and Established Businesses Globally on their Strategic Marketing Initiatives. We welcome the opportunity to offer your Business the chance to build a successful Marketing Strategy to better dominate your market segment quickly and effectively.'
 				/>
 				<meta property='og:url' content='https://www.playhousemedia.net' />
 				<meta
 					property='og:site_name'
-					content='Playhouse Media Digital Marketing Agency'
+					content='Playhouse Media Group Digital Marketing Agency'
 				/>
 				<meta
 					property='article:publisher'
